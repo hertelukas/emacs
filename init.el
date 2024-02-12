7cac3494c266af041796a45df3ce2b9397487667
Lukas <lukas.hertel@tum.de>
2024-02-12 15:27:35 +0100

    which key

--- init.el
+++ init.el
@@ -67,3 +67,10 @@
 ;; rainbow brackets
 (use-package rainbow-delimiters
   :hook (prog-mode . rainbow-delimiters-mode))
+
+;; which key
+(use-package which-key
+  :init (which-key-mode)
+  :diminish  which-key-mode
+  :config
+  (setq which-key-idle-delay 0.2))
